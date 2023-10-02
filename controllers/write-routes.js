3d6aa984bfb267d83df19bcd7791469f64bf006d
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-10-02 14:31:32 -0500

    Update to write

--- controllers/write-routes.js
+++ controllers/write-routes.js
@@ -3,11 +3,11 @@ const withAuth = require('../utils/auth.js')
 const { Post, User } = require('../models')
 
 
-// WRITE NEW BLOG
+// WRITE Task
 router.get('/', withAuth, async (req, res) => res.render('write-post', {userId: req.session.user_id, loggedIn: req.session.loggedIn}))
 
 
-// UPDATE BLOG POST
+// UPDATE Task
 router.get('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id)
@@ -21,7 +21,7 @@ router.get('/:id', withAuth, async (req, res) => {
 
     const post = postData.get({plain: true})
 
-    // user can only edit a post if they are logged in to the account associated with that account
+   
     if (post.user_id !== req.session.user_id) {
       const err = new Error('Please log in to the account that wrote this post.')
       res.status(500).json({message: 'Please log in to the account that wrote this post to edit it.'})
