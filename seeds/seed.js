81d9f373a53eb9d849809209ee8a58a303e23368
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-10-01 23:29:01 -0500

    Removed data
    Removed data causing bugs loading the page

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,7 +3,6 @@ const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
-const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -15,8 +14,6 @@ const seedDatabase = async () => {
   });
   
   await Post.bulkCreate(postData)
-
-  await Comment.bulkCreate(commentData)
 
   process.exit(0);
 };
