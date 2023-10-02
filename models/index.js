3caad6407311cf97bdb46590029f428504582f3d
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-10-02 13:32:44 -0500

    update to model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comment = require('./Comment');
+
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
@@ -11,22 +11,22 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Post.hasMany(Comment, {
+Post.hasMany( {
   foreignKey: 'post_id',
   onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(Post, {
+belongsTo(Post, {
   foreignKey: 'post_id'
 })
 
-User.hasMany(Comment, {
+User.hasMany( {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 })
 
-Comment.belongsTo(User, {
+belongsTo(User, {
   foreignKey: 'user_id',
 })
 
-module.exports = { User, Post, Comment };
+module.exports = { User, Post };
