e2ddf093660a28903d57741264a757afc143c3d5
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-10-02 13:38:57 -0500

    Update to model index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
 
-
 User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
@@ -10,23 +9,5 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
-
-Post.hasMany( {
-  foreignKey: 'post_id',
-  onDelete: 'CASCADE'
-});
-
-belongsTo(Post, {
-  foreignKey: 'post_id'
-})
-
-User.hasMany( {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-})
-
-belongsTo(User, {
-  foreignKey: 'user_id',
-})
 
 module.exports = { User, Post };
